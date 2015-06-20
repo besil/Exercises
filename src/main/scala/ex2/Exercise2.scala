13406cea95027b9cc1846cb6d9c8dc31ed43d3ba
besil <silvio.bernardinello@gmail.com>
2015-06-20 22:14:41 +0200

    First commit

---
+++ src/main/scala/ex2/Exercise2.scala
@@ -0,0 +1,16 @@
+package ex2
+
+/**
+ * Created by besil on 20/06/15.
+ */
+object Exercise2 {
+  /**
+   *
+   * @param num An integer and  is assumed to be at least 0.
+   * @return A list of integers with the first entry being num, and the subsequent ones
+   *         are gotten by omitting the left hand most digit one by one.
+   *         Eg  num=0 returns List(0).
+   *             num=1234 returns List(1234,234,34,4)
+   */
+  def leftTruncate(num:Int):List[Int] = List(3)
+}
---
+++ src/test/scala/ExercisesTests.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FunSuite
+
+/**
+ * Created by besil on 20/06/15.
+ */
+class ExercisesTests extends FunSuite {
+
+}
