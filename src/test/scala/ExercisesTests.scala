92cf9fc5b830954289331341a0ff6c1d8da27c17
besil <silvio.bernardinello@gmail.com>
2015-06-20 22:20:18 +0200

    Added tests

--- src/test/scala/ExercisesTests.scala
+++ src/test/scala/ExercisesTests.scala
@@ -1,8 +1,17 @@
+import ex2.Exercise2
 import org.scalatest.FunSuite
 
 /**
  * Created by besil on 20/06/15.
  */
 class ExercisesTests extends FunSuite {
+
+  test("Case0") {
+    assert(Exercise2.leftTruncate(0) == List(0))
+  }
+
+  test("Case1234") {
+    assert(Exercise2.leftTruncate(1234) == List(1234, 234, 34, 4))
+  }
 
 }
